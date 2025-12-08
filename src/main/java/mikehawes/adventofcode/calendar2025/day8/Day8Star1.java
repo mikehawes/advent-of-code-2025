70566ad1487969286a830039d5da301a921d9705
haradan <haradan@users.noreply.github.com>
2025-12-08 18:34:51 +0000

    Set up test for day 8 star 1

---
+++ src/main/java/mikehawes/adventofcode/calendar2025/day8/Day8Star1.java
@@ -0,0 +1,7 @@
+package mikehawes.adventofcode.calendar2025.day8;
+
+public class Day8Star1 {
+    public static long joinBoxesMultiplyLargestCircuitLengths(String input, int joins, int multiplyCircuits) {
+        return 0;
+    }
+}
