7e2075827a2c99bea0dbc7215455c7f485ec1e3a
haradan <haradan@users.noreply.github.com>
2025-12-08 18:33:00 +0000

    Find n shortest connections

--- src/main/java/mikehawes/adventofcode/calendar2025/day8/Connection.java
+++ src/main/java/mikehawes/adventofcode/calendar2025/day8/Connection.java
@@ -7,21 +7,24 @@ import java.util.PriorityQueue;
 
 public record Connection(Point from, Point to, double distance) {
 
-    public static List<Connection> findShortest(List<Point> points) {
+    public static List<Connection> findShortest(int limit, List<Point> points) {
         PriorityQueue<Connection> connections = new PriorityQueue<>(
-                Comparator.comparing(Connection::distance));
+                Comparator.comparing(Connection::distance).reversed());
         for (int i = 0; i < points.size(); i++) {
             Point from = points.get(i);
             for (int j = i + 1; j < points.size(); j++) {
                 Point to = points.get(j);
                 double distance = from.findDistanceTo(to);
                 connections.add(new Connection(from, to, distance));
+                if(connections.size() > limit) {
+                    connections.poll();
+                }
             }
         }
         List<Connection> order = new ArrayList<>(connections.size());
         for (Connection connection = connections.poll(); connection != null; connection = connections.poll()) {
             order.add(connection);
         }
-        return order;
+        return order.reversed();
     }
 }
--- src/test/java/mikehawes/adventofcode/calendar2025/day8/ConnectionTest.java
+++ src/test/java/mikehawes/adventofcode/calendar2025/day8/ConnectionTest.java
@@ -15,9 +15,39 @@ public class ConnectionTest {
                 0,0,10
                 0,0,100
                 """);
-        assertThat(Connection.findShortest(points)).containsExactly(
+        assertThat(Connection.findShortest(3, points)).containsExactly(
                 new Connection(new Point(0, 0, 1), new Point(0, 0, 10), 9),
                 new Connection(new Point(0, 0, 10), new Point(0, 0, 100), 90),
                 new Connection(new Point(0, 0, 1), new Point(0, 0, 100), 99));
+    }
+
+    @Test
+    void should_find_more_connections() {
+        List<Point> points = Point.listFrom("""
+                0,0,1
+                0,0,10
+                0,0,50
+                0,0,100
+                """);
+        assertThat(Connection.findShortest(10, points)).containsExactly(
+                new Connection(new Point(0, 0, 1), new Point(0, 0, 10), 9),
+                new Connection(new Point(0, 0, 10), new Point(0, 0, 50), 40),
+                new Connection(new Point(0, 0, 1), new Point(0, 0, 50), 49),
+                new Connection(new Point(0, 0, 50), new Point(0, 0, 100), 50),
+                new Connection(new Point(0, 0, 10), new Point(0, 0, 100), 90),
+                new Connection(new Point(0, 0, 1), new Point(0, 0, 100), 99));
+    }
+
+    @Test
+    void should_limit_shortest() {
+        List<Point> points = Point.listFrom("""
+                0,0,1
+                0,0,10
+                0,0,50
+                0,0,100
+                """);
+        assertThat(Connection.findShortest(2, points)).containsExactly(
+                new Connection(new Point(0, 0, 1), new Point(0, 0, 10), 9),
+                new Connection(new Point(0, 0, 10), new Point(0, 0, 50), 40));
     }
 }
